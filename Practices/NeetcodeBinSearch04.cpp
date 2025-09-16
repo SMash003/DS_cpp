e23ebea677055dbbe19ece870dc2f3e86b34b893
Md. Shahraj Mashrafe Mashfe <shahrajmashfe@gmail.com>
2025-09-16 22:23:08 +0600

    Min of rotated sorted array

---
+++ Practices/NeetcodeBinSearch04.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findMin(vector<int> &nums) {
+        int res = nums[0];
+        int l = 0, r = nums.size() - 1;
+        while (l <= r)
+        {
+            if(nums[l] < nums[r])
+            {
+                res = min(res, nums[l]);
+                break;
+            }
+            int mid = (l+r)/2;
+            res = min(res, nums[mid]);
+            if(nums[mid] >= nums[l])
+            {
+                l = mid + 1;
+            }
+            else
+                r = mid - 1;
+        }
+        return res;
+    }
+};
+
+int main(){
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    cout << "Enter elements of array: ";
+    vector<int> nums(n);
+    for(int i=0; i<n; i++)
+    {
+        cin >> nums[i];
+    }
+    Solution sol;
+    cout << sol.findMin(nums);
+}
